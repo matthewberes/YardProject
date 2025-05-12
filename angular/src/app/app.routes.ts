cc6c4e1b43f557e734e4dc654085ba4f2a9b3d6f
Matt Beres <203850019+rcdsb-matt-beres@users.noreply.github.com>
2025-05-12 15:55:10 -0400

    add taskbar

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { PlantComponent } from './plant/plant.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: IndexComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'index', component: IndexComponent },
     { path: 'index/:plant', component: PlantComponent }
 ];
--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) { }
+
+  path: string = "";
+
+  ngOnInit(): void {
+    if (window.location.hash == "") {
+      this.path = "/home";
+    } else {
+      this.path = window.location.hash;
+    }
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((ev: NavigationEnd) => {
+        if (ev.url == "/") {
+          this.path = "/home";
+        } else {
+          this.path = "" + ev.url
+        }
+      });
+  }
 }
