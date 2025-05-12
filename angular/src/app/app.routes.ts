7624adb64455bbb7e4978f21387fa8192e8ff6af
Matt Beres <203850019+rcdsb-matt-beres@users.noreply.github.com>
2025-05-12 15:13:35 -0400

    some ui upgrades

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { PlantComponent } from './plant/plant.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: IndexComponent },
+    { path: 'index', component: IndexComponent },
+    { path: 'index/:plant', component: PlantComponent }
+];
---
+++ angular/src/app/plant/plant.component.ts
@@ -0,0 +1,29 @@
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-plant',
+  imports: [NgIf],
+  templateUrl: './plant.component.html',
+  styleUrl: './plant.component.css'
+})
+export class PlantComponent implements OnInit {
+  constructor(private route: ActivatedRoute) { }
+
+  plantList = ["Dandelion", "Hawkweed", "Hedge Bedstraw", "Creeping Charlie"];
+  plantParam: string;
+  currPlant: string;
+  set404: boolean = false;
+
+  ngOnInit(): void {
+    this.plantParam = this.route.snapshot.paramMap.get('plant');
+    if (this.plantList.includes(this.plantParam)) {
+      this.currPlant = this.plantParam;
+    } else {
+      this.currPlant = "Plant not found...";
+      this.set404 = true;
+    }
+
+  }
+}
