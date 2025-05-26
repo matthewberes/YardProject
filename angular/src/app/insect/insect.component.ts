1e65986fa065997c3927c2ab2a2e0cbf4554a94f
Matt Beres <mattyberes@gmail.com>
2025-05-26 15:53:24 -0400

    add insect and insecticide skeleton

--- angular/src/app/insect/insect.component.ts
+++ angular/src/app/insect/insect.component.ts
@@ -1,11 +1,45 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { InsectService } from '../insect.service';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-insect',
-  imports: [],
+  imports: [NgIf, CdkAccordionModule],
   templateUrl: './insect.component.html',
   styleUrl: './insect.component.css'
 })
 export class InsectComponent {
+  constructor(private route: ActivatedRoute, private insectService: InsectService, private http: HttpClient) { }
 
+  insectParam: string;
+  currInsect: any = "Insect not found...";
+  set404: boolean = true;
+  accordionTitles: string[] = ["Identification", "Removal", "Origins", "Life cycle", "Notes"];
+  expandedIndex = 0;
+
+  ngOnInit(): void {
+    if (this.insectService.currInsect.value) {
+      this.currInsect = this.insectService.currInsect.value;
+      this.set404 = false
+    } else {
+      this.insectParam = this.route.snapshot.paramMap.get('insect');
+      this.http.get<any>('insect-index.json').subscribe(data => {
+        let insects = data.insects;
+        insects.forEach(insect => {
+          if (insect.link == this.insectParam) {
+            this.currInsect = insect
+            this.set404 = false
+          }
+        })
+      });
+    }
+  }
+
+  goBack(): void {
+    this.insectService.currInsect.next(null);
+    window.history.back();
+  }
 }
--- angular/src/app/insecticide/insecticide.component.ts
+++ angular/src/app/insecticide/insecticide.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InsecticideService } from '../insecticide.service';
+import { HttpClient } from '@angular/common/http';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-insecticide',
-  imports: [],
+  imports: [NgIf, CdkAccordionModule],
   templateUrl: './insecticide.component.html',
   styleUrl: './insecticide.component.css'
 })
-export class InsecticideComponent {
+export class InsecticideComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private insecticideService: InsecticideService, private http: HttpClient) { }
 
+  insecticideParam: string;
+  currInsecticide: any = "Insecticide not found...";
+  set404: boolean = true;
+  accordionTitles: string[] = ["Application", "Legality", "Pros", "Cons", "Notes"];
+  expandedIndex = 0;
+
+  ngOnInit(): void {
+    if (this.insecticideService.currInsecticide.value) {
+      this.currInsecticide = this.insecticideService.currInsecticide.value;
+      this.set404 = false
+    } else {
+      this.insecticideParam = this.route.snapshot.paramMap.get('insecticide');
+      this.http.get<any>('insecticide-index.json').subscribe(data => {
+        let insecticides = data.insecticides;
+        insecticides.forEach(insecticide => {
+          if (insecticide.link == this.insecticideParam) {
+            this.currInsecticide = insecticide
+            this.set404 = false
+          }
+        })
+      });
+    }
+  }
+
+  goBack(): void {
+    this.insecticideService.currInsecticide.next(null);
+    window.history.back();
+  }
 }
