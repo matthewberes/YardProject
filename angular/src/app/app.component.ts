9a657c9b67e5a4b2d787e326259aa768ec6a2f51
Matt Beres <mattyberes@gmail.com>
2025-08-12 16:06:47 -0400

    make mobile header appear and disappear

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { SideComponent } from './side/side.component';
 import { AdComponent } from './ad/ad.component';
 import { FooterComponent } from './footer/footer.component';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,25 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   title = 'angular';
+  lastScrollTop = 0;
+  showHeader = true;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (screen.width > 600) {
+      this.showHeader = true;
+      return;
+    }
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st < 10) {
+      this.showHeader = true;
+    } else if (st > this.lastScrollTop) {
+      // Scrolling down
+      this.showHeader = false;
+    } else {
+      // Scrolling up
+      this.showHeader = true;
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st;
+  }
 }
